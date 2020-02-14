9fcdf107b33995e6a7e6c9bbe4abe6f711bca3e6
Oracle Public Cloud User <opc@entry2.subnet.vcn.oraclevcn.com>
2020-02-14 10:24:47 +0000

    connections neues Layout

--- client/connections.js
+++ client/connections.js
@@ -7,30 +7,52 @@
 "use strict";
 
 
+function getMidPoint (id) {
+    var elem = document.getElementById(id);
+    
+    if (id.startsWith("server")) {
+        return { "left": elem.getBoundingClientRect().left + window.pageXOffset + 30,
+                 "top" : elem.getBoundingClientRect().top  + window.pageYOffset + 10 };
+    } else if (id.startsWith("group_replication"))  {
+	console.log ("Connection to a group_replication should not happen! See "+id);
+        return { "left": elem.getBoundingClientRect().left + window.pageXOffset + (elem.scrollWidth /2),
+                 "top" : elem.getBoundingClientRect().top  + window.pageYOffset + (elem.scrollHeight/2)};
+    } else if (id.startsWith("load_generator")) {
+	return { "left": elem.getBoundingClientRect().left + window.pageXOffset + 20,
+                 "top" : elem.getBoundingClientRect().top  + window.pageYOffset + 3 };
+    } else if (id.startsWith("router")) {
+        return {"left": elem.getBoundingClientRect().left + window.pageXOffset + 40,
+                "top" : elem.getBoundingClientRect().top  + window.pageYOffset + 5 };
+    } else if (id.startsWith("tpsmeter")) {
+        return {"left": elem.getBoundingClientRect().left + window.pageXOffset + 40,
+                "top" : elem.getBoundingClientRect().top  + window.pageYOffset + 30 };
+    }
+    console.log("Connection to unknown element: "+id);
+    return { "left": elem.getBoundingClientRect().left + window.pageXOffset + (elem.scrollWidth /2),
+             "top" : elem.getBoundingClientRect().top  + window.pageYOffset + (elem.scrollHeight/2)};
+}
+
 
 function displayConnection(ci) {
     // only display connection if not yet present
     if (document.getElementById(ci["id"]) !== null) return;
     
-    var from = $("#"+ci["from"]);
-    var to   = $("#"+ci["to"]);
+//    var from = $("#"+ci["from"]);
+//    var to   = $("#"+ci["to"]);
+    var mid1 = getMidPoint(ci["from"]);
+    var mid2 = getMidPoint(ci["to"]);
     var root = $("#main");
-    
-    var x1 = from.position().left + (parseInt(from.css("width")) /2);
-    var y1 = from.position().top  + (parseInt(from.css("height"))/2);
-    var x2 =   to.position().left + (parseInt(  to.css("width")) /2);
-    var y2 =   to.position().top  + (parseInt(  to.css("height"))/2);
-    
+   
     var angle;
-    if (x1===x2 && y1<y2)
+    if (mid1.left === mid2.left && mid1.top < mid2.top)
         angle=90;
-    else if (x1===x2 && y1>=y2)
+    else if (mid1.left === mid2.left && mid1.top >= mid2.top)
         angle=-90;
-    else if (x2>x1)
-        angle=Math.atan((y2-y1)/(x2-x1))*360/2/Math.PI;
-    else //x2<=x1
-        angle=Math.atan((y2-y1)/(x2-x1))*360/2/Math.PI+180;
-    var length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+    else if (mid2.left > mid1.left)
+        angle=Math.atan((mid2.top - mid1.top)/(mid2.left - mid1.left))*360/2/Math.PI;
+    else //mid2.left <= mid1.left
+        angle=Math.atan((mid2.top - mid1.top)/(mid2.left - mid1.left))*360/2/Math.PI+180;
+    var length = Math.sqrt(Math.pow((mid2.left - mid1.left),2) + Math.pow((mid2.top - mid1.top),2));
     
     var arrow = document.createElementNS('http://www.w3.org/2000/svg','svg');
     arrow.setAttribute("id", ci["id"]);
@@ -39,32 +61,32 @@ function displayConnection(ci) {
     if(ci["type"]===1) {
         // Drawing a replication line
         var line1 = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line1.setAttribute("x1",x1);
-        line1.setAttribute("y1",y1-5);
-        line1.setAttribute("x2",x1+length);
-        line1.setAttribute("y2",y1-5);
-        line1.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line1.setAttribute("x1",mid1.left);
+        line1.setAttribute("y1",mid1.top - 5);
+        line1.setAttribute("x2",mid1.left + length);
+        line1.setAttribute("y2",mid1.top - 5);
+        line1.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line1.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         var line2 = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line2.setAttribute("x1",x1);
-        line2.setAttribute("y1",y1+5);
-        line2.setAttribute("x2",x1+length);
-        line2.setAttribute("y2",y1+5);
-        line2.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line2.setAttribute("x1",mid1.left);
+        line2.setAttribute("y1",mid1.top + 5);
+        line2.setAttribute("x2",mid1.left + length);
+        line2.setAttribute("y2",mid1.top + 5);
+        line2.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line2.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         var line4 = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line4.setAttribute("x1",x1+(length/2));
-        line4.setAttribute("y1",y1);
-        line4.setAttribute("x2",x1+(length/2)-5);
-        line4.setAttribute("y2",y1-5);
-        line4.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line4.setAttribute("x1",mid1.left+(length/2));
+        line4.setAttribute("y1",mid1.top);
+        line4.setAttribute("x2",mid1.left+(length/2)-5);
+        line4.setAttribute("y2",mid1.top-5);
+        line4.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line4.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         var line3 = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line3.setAttribute("x1",x1+(length/2));
-        line3.setAttribute("y1",y1);
-        line3.setAttribute("x2",x1+(length/2)-5);
-        line3.setAttribute("y2",y1+5);
-        line3.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line3.setAttribute("x1",mid1.left+(length/2));
+        line3.setAttribute("y1",mid1.top);
+        line3.setAttribute("x2",mid1.left+(length/2)-5);
+        line3.setAttribute("y2",mid1.top+5);
+        line3.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line3.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         arrow.appendChild(line1);
         arrow.appendChild(line2);
@@ -72,25 +94,25 @@ function displayConnection(ci) {
         arrow.appendChild(line4);
     } else {// client connection
         var line = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line.setAttribute("x1",x1);
-        line.setAttribute("y1",y1);
-        line.setAttribute("x2",x1+length);
-        line.setAttribute("y2",y1);
-        line.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line.setAttribute("x1",mid1.left);
+        line.setAttribute("y1",mid1.top);
+        line.setAttribute("x2",mid1.left + length);
+        line.setAttribute("y2",mid1.top);
+        line.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         var line2 = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line2.setAttribute("x1",x1+(length/2));
-        line2.setAttribute("y1",y1);
-        line2.setAttribute("x2",x1+(length/2)-5);
-        line2.setAttribute("y2",y1-5);
-        line2.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line2.setAttribute("x1",mid1.left + (length/2));
+        line2.setAttribute("y1",mid1.top);
+        line2.setAttribute("x2",mid1.left + (length/2)-5);
+        line2.setAttribute("y2",mid1.top - 5);
+        line2.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line2.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         var line3 = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line3.setAttribute("x1",x1+(length/2));
-        line3.setAttribute("y1",y1);
-        line3.setAttribute("x2",x1+(length/2)-5);
-        line3.setAttribute("y2",y1+5);
-        line3.setAttribute("transform", "rotate("+angle+","+x1+","+y1+")");
+        line3.setAttribute("x1",mid1.left + (length/2));
+        line3.setAttribute("y1",mid1.top);
+        line3.setAttribute("x2",mid1.left + (length/2)-5);
+        line3.setAttribute("y2",mid1.top + 5);
+        line3.setAttribute("transform", "rotate("+angle+","+mid1.left+","+mid1.top+")");
         line3.setAttribute("style","stroke:rgb(30,30,30);stroke-width:2");
         arrow.appendChild(line);
         arrow.appendChild(line2);
--- client/tpsmeter.js
+++ client/tpsmeter.js
@@ -13,15 +13,43 @@ function createTpsMeter(el) {
     var id = el["id"];
     var tpsdiv = document.createElementNS('http://www.w3.org/2000/svg','svg');
     tpsdiv.setAttribute("id", id);
-    tpsdiv.setAttribute("style", "position: absolute; left: "+el["xPos"]+"; top: "+el["yPos"]+"px; width: 102px; height: 102px");
+    tpsdiv.setAttribute("style", "position: absolute; left: "+el["xPos"]+"px; top: "+el["yPos"]+"px; width: 104px; height: 104px");
+    var box = document.createElementNS('http://www.w3.org/2000/svg','rect');
+    box.setAttribute("style", "fill:rgb(230,230,250);stroke-width:2;stroke:rgb(0,0,0)");
+    box.setAttribute("x", "0");
+    box.setAttribute("y", "0");
+    box.setAttribute("width", "104");
+    box.setAttribute("height", "104");
+    tpsdiv.append(box);
+    var label1 = document.createElementNS('http://www.w3.org/2000/svg','text');
+    label1.setAttribute("style", "fill:rgb(0,0,0); font-size: 12px; font-family: sans-serif");
+    label1.setAttribute("x", "5");
+    label1.setAttribute("y", "15");
+    label1.setAttribute("width", "104");
+    label1.setAttribute("height", "10");
+    label1.setAttribute("id", id+"_tpslabel");
+    tpsdiv.append(label1);
+    var label2 = document.createElementNS('http://www.w3.org/2000/svg','text');
+    label2.setAttribute("style", "fill:rgb(0,0,0); font-size: 12px; font-family: sans-serif");
+    label2.setAttribute("x", "5");
+    label2.setAttribute("y", "98");
+    label2.setAttribute("width", "104");
+    label2.setAttribute("height", "10");
+    label2.setAttribute("id", id+"_hostlabel");
+    tpsdiv.append(label2);
+    var title=document.createElementNS('http://www.w3.org/2000/svg','title');
+    title.setAttribute("id", id+"_title");
+    tpsdiv.append(title);
+
     var line;
     for(var i=0; i<BUFSIZE; ++i){
         line = document.createElementNS('http://www.w3.org/2000/svg','line');
-        line.setAttribute("x1",el["xPos"]+i+1);
-        line.setAttribute("y1",el["yPos"]+101);
-        line.setAttribute("x2",el["xPos"]+i+1);
-        line.setAttribute("y2",el["yPos"]+101);
+        line.setAttribute("x1",i+2);
+        line.setAttribute("y1",102);
+        line.setAttribute("x2",i+2);
+        line.setAttribute("y2",102);
         line.setAttribute("id", id+"_"+i)
+        line.setAttribute("style","stroke:rgb(120,120,120);stroke-width:1");
         tpsdiv.append(line);
     }
     return tpsdiv;
@@ -35,8 +63,18 @@ function updateTpsMeter(domItem, el) {
         document.getElementById("main").appendChild(domItem);
     }
     var line;
+    var id = el["id"]+"_";
+
+    document.getElementById(id+"title").textContent = "Last error: "+el["lastSqlError"]; 
+    document.getElementById(id+"tpslabel").textContent = "max "+el["maxTps"]+"tps";   
+    if (el["connected"]) 
+        document.getElementById(id+"hostlabel").textContent = el["hostname"];
+    else
+        document.getElementById(id+"hostlabel").textContent = "N/C for "+el["waittime"]+"s";
     for (var i=0; i<BUFSIZE; ++i) {
-        line = document.getElementById(el["id"]+_i);
-        line.setAttribute("y2",el["yPos"]+101-el["tps"][i]);
+        line = document.getElementById(id+i);
+        line.setAttribute("y2",100-(el["tps"][i]*100/el["maxTps"]));
+//        if(i===99)
+//          console.log("tps(i)="+el["tps"][i]+" "+line.getAttribute("id")+": "+line.getAttribute("y2"));
     }
 }
